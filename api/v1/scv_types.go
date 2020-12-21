47401c0d23b420aade5dc36e99bb0e3e11aa2ba9
self-cream <439450110@qq.com>
2020-12-21 10:44:18 +0800

    add core number

--- api/v1/scv_types.go
+++ api/v1/scv_types.go
@@ -57,15 +57,16 @@ func (in CardList) Swap(i, j int) {
 }
 
 type Card struct {
-	ID              uint   `json:"id"`
-	Health          string `json:"health,omitempty"`
-	Model           string `json:"model,omitempty"`
-	Power           uint   `json:"power,omitempty"`
-	TotalMemory     uint64 `json:"totalMemory,omitempty"`
-	Clock           uint   `json:"clock,omitempty"`
-	FreeMemory      uint64 `json:"freeMemory,omitempty"`
-	Core            uint   `json:"core,omitempty"`
-	Bandwidth       uint   `json:"bandwidth,omitempty"`
+	ID              uint     `json:"id"`
+	Health          string   `json:"health,omitempty"`
+	Model           string   `json:"model,omitempty"`
+	Power           uint     `json:"power,omitempty"`
+	TotalMemory     uint64   `json:"totalMemory,omitempty"`
+	Clock           uint     `json:"clock,omitempty"`
+	FreeMemory      uint64   `json:"freeMemory,omitempty"`
+	Core            uint     `json:"core,omitempty"`
+	Bandwidth       uint     `json:"bandwidth,omitempty"`
+	CoreNumber      uint     `json:"coreNumber"`
 	AffinityTag     []string `json:"affinityTags"`
 	AntiAffinityTag []string `json:"antiAffinityTags"`
 	ExclusionTag    []string `json:"exclusionTags"`
